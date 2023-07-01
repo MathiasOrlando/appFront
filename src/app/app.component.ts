bc1aeb323ed909665164599ea8f4aded5fbb7fb4
Mi nombre <mthmcl@gmail.com>
2023-07-01 08:37:21 -0400

    Changes to be committed:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'examen';
+  goToConsultarCiudad = 'consultarciudad';
+  title = 'App Conexión Java';
 }
+
+
